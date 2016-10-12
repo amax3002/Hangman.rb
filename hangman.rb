bcc0e0527d23094eac7f1d372e286c3d0c000159
Alexandre Guy <alexandremguy@AGuysBook.local>
2016-10-12 17:23:26 -0400

    End of day saving

--- hangman.rb
+++ hangman.rb
@@ -2,17 +2,30 @@
 alphabet = ("a".."z").to_a
 do_you_want_to_play = "Yes"
 puts ''
-
+#puts File.read('/usr/share/dict/words')
 while do_you_want_to_play == "Yes"
-  word_choice = ["banana","obama","pie","adele","submarine"]
-  chosen_word = word_choice.sample
-  length_word = chosen_word.length.to_i
-  max_number_of_guess = 6
+  word_choice = File.read('/usr/share/dict/words').lines.select {|l| (4..9).cover?(l.strip.size)}.sample.strip
+  length_word = word_choice.length.to_i
+  max_number_of_guess = 0
   user_picks = []
-  letters_of_word_choice = chosen_word.split("")
+  letters_of_word_choice = word_choice.split("")
   inbetween_array = []
   storing_array = []
   finish = 0
+  counter_guess_correct = []
+
+def difficulty()
+  puts "How many guesses would you like? (leaving it blank makes it 9)"
+  choice = gets.chomp.to_i
+  if choice == nil || choice == '' || choice == 0
+    choice = 9
+  else
+  end
+  return choice
+end
+
+max_number_of_guess = difficulty()
+
 
 def create_fill_out(guesses, chars)
   masked_characters = []
@@ -27,6 +40,19 @@ def create_fill_out(guesses, chars)
   return masked_characters.join(" ")
 
 end
+
+#
+# def correct_guesses_left(guesses, input, correct_guess)
+#     if guesses.include?(input)
+#       correct_guess += 1
+#
+#     else
+#       correct_guess
+#     end
+#   return correct_guess
+#
+# end
+
 
 
 
@@ -56,8 +82,6 @@ def no_double_guesses(guesses, input, guess_count, valid_input)
   end
   return guess_count
 end
-
-
 
 def end_game(guesses, answer, guesses_final, finish, word_picked)
   if guesses == answer.join(" ")
@@ -96,19 +120,26 @@ while max_number_of_guess != 0 && finish != 1
   puts "Please submit another guess:"
 
   input = gets.chomp
+  #max_number_of_guess = correct_guesses_left(inbetween_array,input,max_number_of_guess)
   puts ''
   max_number_of_guess = no_double_guesses(user_picks, input, max_number_of_guess, alphabet)
 
 
   puts create_fill_out(user_picks,letters_of_word_choice)
-  max_number_of_guess = inbetween_array = create_fill_out(user_picks,letters_of_word_choice)
+  inbetween_array = create_fill_out(user_picks,letters_of_word_choice)
+  if counter_guess_correct != inbetween_array && counter_guess_correct.empty? != true
+    max_number_of_guess+=1
+  else
+  end
+
+  counter_guess_correct = inbetween_array
 
 
   max_number_of_guess -= 1
   puts ''
 
 
-  finish = end_game(inbetween_array, letters_of_word_choice, max_number_of_guess, finish, chosen_word)
+  finish = end_game(inbetween_array, letters_of_word_choice, max_number_of_guess, finish, word_choice)
   puts ''
 end
 
